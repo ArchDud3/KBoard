8bb343cde7bf3d84aaa66817414f44667028c25a
ArchDud3 <albrechtarcher@gmail.com>
2023-11-20 20:06:17 +1100

    I finished, look at me go

---
+++ src/js/newkey.js
@@ -0,0 +1,8 @@
+c6s = document.querySelector("#c6s"); let c6ssound = new Audio('/src/sounds/Db6.mp3'); 
+c6s.addEventListener("click", function() {c6ssound.play();}); c6s.addEventListener("mouseup", function() {c6ssound.pause();c6ssound.load();}) 
+d6s = document.querySelector("#d6s"); let d6ssound = new Audio('/src/sounds/Eb6.mp3'); 
+d6s.addEventListener("click", function() {d6ssound.play();}); d6s.addEventListener("mouseup", function() {d6ssound.pause();d6ssound.load();}) 
+f6s = document.querySelector("#f6s"); let f6ssound = new Audio('/src/sounds/Gb6.mp3'); 
+f6s.addEventListener("click", function() {f6ssound.play();}); f6s.addEventListener("mouseup", function() {f6ssound.pause();f6ssound.load();})  
+g6s = document.querySelector("#g6s"); let g6ssound = new Audio('/src/sounds/Ab6.mp3'); 
+g6s.addEventListener("click", function() {g6ssound.play();}); g6s.addEventListener("mouseup", function() {g6ssound.pause();g6ssound.load();})
---
+++ src/js/piano.js
@@ -0,0 +1,164 @@
+
+// OCTAVE 1, LINES 3-16
+a1 = document.querySelector("#a1"); let a1sound = new Audio('/src/sounds/A1.mp3'); 
+a1.addEventListener("click", function() {a1sound.play();}); a1.addEventListener("mouseup", function() {a1sound.pause();a1sound.load();}) 
+b1 = document.querySelector("#b1"); let b1sound = new Audio('/src/sounds/B1.mp3'); 
+b1.addEventListener("click", function() {b1sound.play();}); b1.addEventListener("mouseup", function() {b1sound.pause();b1sound.load();}) 
+c1 = document.querySelector("#c1"); let c1sound = new Audio('/src/sounds/C1.mp3'); 
+c1.addEventListener("click", function() {c1sound.play();}); c1.addEventListener("mouseup", function() {c1sound.pause();c1sound.load();})  
+d1 = document.querySelector("#d1"); let d1sound = new Audio('/src/sounds/D1.mp3'); 
+d1.addEventListener("click", function() {d1sound.play();}); d1.addEventListener("mouseup", function() {d1sound.pause();d1sound.load();})
+e1 = document.querySelector("#e1"); let e1sound = new Audio('/src/sounds/E1.mp3'); 
+e1.addEventListener("click", function() {e1sound.play();}); e1.addEventListener("mouseup", function() {e1sound.pause();e1sound.load();})
+f1 = document.querySelector("#f1"); let f1sound = new Audio('/src/sounds/F1.mp3'); 
+f1.addEventListener("click", function() {f1sound.play();}); f1.addEventListener("mouseup", function() {f1sound.pause();f1sound.load();})  
+g1 = document.querySelector("#g1"); let g1sound = new Audio('/src/sounds/G1.mp3'); 
+g1.addEventListener("click", function() {g1sound.play();}); g1.addEventListener("mouseup", function() {g1sound.pause();g1sound.load();})  
+
+// OCTAVE 2, LINES 19-32
+a2 = document.querySelector("#a2"); let a2sound = new Audio('/src/sounds/A2.mp3'); 
+a2.addEventListener("click", function() {a2sound.play();}); a2.addEventListener("mouseup", function() {a2sound.pause();a2sound.load();}) 
+b2 = document.querySelector("#b2"); let b2sound = new Audio('/src/sounds/B2.mp3'); 
+b2.addEventListener("click", function() {b2sound.play();}); b2.addEventListener("mouseup", function() {b2sound.pause();b2sound.load();}) 
+c2 = document.querySelector("#c2"); let c2sound = new Audio('/src/sounds/C2.mp3'); 
+c2.addEventListener("click", function() {c2sound.play();}); c2.addEventListener("mouseup", function() {c2sound.pause();c2sound.load();})  
+d2 = document.querySelector("#d2"); let d2sound = new Audio('/src/sounds/D2.mp3'); 
+d2.addEventListener("click", function() {d2sound.play();}); d2.addEventListener("mouseup", function() {d2sound.pause();d2sound.load();})
+e2 = document.querySelector("#e2"); let e2sound = new Audio('/src/sounds/E2.mp3'); 
+e2.addEventListener("click", function() {e2sound.play();}); e2.addEventListener("mouseup", function() {e2sound.pause();e2sound.load();})
+f2 = document.querySelector("#f2"); let f2sound = new Audio('/src/sounds/F2.mp3'); 
+f2.addEventListener("click", function() {f2sound.play();}); f2.addEventListener("mouseup", function() {f2sound.pause();f2sound.load();})  
+g2 = document.querySelector("#g2"); let g2sound = new Audio('/src/sounds/G2.mp3'); 
+g2.addEventListener("click", function() {g2sound.play();}); g2.addEventListener("mouseup", function() {g2sound.pause();g2sound.load();})
+
+// OCTAVE 3, LINES 35-48
+a3 = document.querySelector("#a3"); let a3sound = new Audio('/src/sounds/A3.mp3'); 
+a3.addEventListener("click", function() {a3sound.play();}); a3.addEventListener("mouseup", function() {a3sound.pause();a3sound.load();}) 
+b3 = document.querySelector("#b3"); let b3sound = new Audio('/src/sounds/B3.mp3'); 
+b3.addEventListener("click", function() {b3sound.play();}); b3.addEventListener("mouseup", function() {b3sound.pause();b3sound.load();}) 
+c3 = document.querySelector("#c3"); let c3sound = new Audio('/src/sounds/C3.mp3'); 
+c3.addEventListener("click", function() {c3sound.play();}); c3.addEventListener("mouseup", function() {c3sound.pause();c3sound.load();})  
+d3 = document.querySelector("#d3"); let d3sound = new Audio('/src/sounds/D3.mp3'); 
+d3.addEventListener("click", function() {d3sound.play();}); d3.addEventListener("mouseup", function() {d3sound.pause();d3sound.load();})
+e3 = document.querySelector("#e3"); let e3sound = new Audio('/src/sounds/E3.mp3'); 
+e3.addEventListener("click", function() {e3sound.play();}); e3.addEventListener("mouseup", function() {e3sound.pause();e3sound.load();})
+f3 = document.querySelector("#f3"); let f3sound = new Audio('/src/sounds/F3.mp3'); 
+f3.addEventListener("click", function() {f3sound.play();}); f3.addEventListener("mouseup", function() {f3sound.pause();f3sound.load();})  
+g3 = document.querySelector("#g3"); let g3sound = new Audio('/src/sounds/G3.mp3'); 
+g3.addEventListener("click", function() {g3sound.play();}); g3.addEventListener("mouseup", function() {g3sound.pause();g3sound.load();})
+
+// OCTAVE 4, LINES 51-64
+a4 = document.querySelector("#a4"); let a4sound = new Audio('/src/sounds/A4.mp3'); 
+a4.addEventListener("click", function() {a4sound.play();}); a4.addEventListener("mouseup", function() {a4sound.pause();a4sound.load();}) 
+b4 = document.querySelector("#b4"); let b4sound = new Audio('/src/sounds/B4.mp3'); 
+b4.addEventListener("click", function() {b4sound.play();}); b4.addEventListener("mouseup", function() {b4sound.pause();b4sound.load();}) 
+c4 = document.querySelector("#c4"); let c4sound = new Audio('/src/sounds/C4.mp3'); 
+c4.addEventListener("click", function() {c4sound.play();}); c4.addEventListener("mouseup", function() {c4sound.pause();c4sound.load();})  
+d4 = document.querySelector("#d4"); let d4sound = new Audio('/src/sounds/D4.mp3'); 
+d4.addEventListener("click", function() {d4sound.play();}); d4.addEventListener("mouseup", function() {d4sound.pause();d4sound.load();})
+e4 = document.querySelector("#e4"); let e4sound = new Audio('/src/sounds/E4.mp3'); 
+e4.addEventListener("click", function() {e4sound.play();}); e4.addEventListener("mouseup", function() {e4sound.pause();e4sound.load();})
+f4 = document.querySelector("#f4"); let f4sound = new Audio('/src/sounds/F4.mp3'); 
+f4.addEventListener("click", function() {f4sound.play();}); f4.addEventListener("mouseup", function() {f4sound.pause();f4sound.load();})  
+g4 = document.querySelector("#g4"); let g4sound = new Audio('/src/sounds/G4.mp3'); 
+g4.addEventListener("click", function() {g4sound.play();}); g4.addEventListener("mouseup", function() {g4sound.pause();g4sound.load();})  
+
+// OCTAVE 5, LINES 67-80
+a5 = document.querySelector("#a5"); let a5sound = new Audio('/src/sounds/A5.mp3'); 
+a5.addEventListener("click", function() {a5sound.play();}); a5.addEventListener("mouseup", function() {a5sound.pause();a5sound.load();}) 
+b5 = document.querySelector("#b5"); let b5sound = new Audio('/src/sounds/B5.mp3'); 
+b5.addEventListener("click", function() {b5sound.play();}); b5.addEventListener("mouseup", function() {b5sound.pause();b5sound.load();}) 
+c5 = document.querySelector("#c5"); let c5sound = new Audio('/src/sounds/C5.mp3'); 
+c5.addEventListener("click", function() {c5sound.play();}); c5.addEventListener("mouseup", function() {c5sound.pause();c5sound.load();})  
+d5 = document.querySelector("#d5"); let d5sound = new Audio('/src/sounds/D5.mp3'); 
+d5.addEventListener("click", function() {d5sound.play();}); d5.addEventListener("mouseup", function() {d5sound.pause();d5sound.load();})
+e5 = document.querySelector("#e5"); let e5sound = new Audio('/src/sounds/E5.mp3'); 
+e5.addEventListener("click", function() {e5sound.play();}); e5.addEventListener("mouseup", function() {e5sound.pause();e5sound.load();})
+f5 = document.querySelector("#f5"); let f5sound = new Audio('/src/sounds/F5.mp3'); 
+f5.addEventListener("click", function() {f5sound.play();}); f5.addEventListener("mouseup", function() {f5sound.pause();f5sound.load();})  
+g5 = document.querySelector("#g5"); let g5sound = new Audio('/src/sounds/G5.mp3'); 
+g5.addEventListener("click", function() {g5sound.play();}); g5.addEventListener("mouseup", function() {g5sound.pause();g5sound.load();})
+
+// OCTAVE 6, LINES 83-96
+a6 = document.querySelector("#a6"); let a6sound = new Audio('/src/sounds/A6.mp3'); 
+a6.addEventListener("click", function() {a6sound.play();}); a6.addEventListener("mouseup", function() {a6sound.pause();a6sound.load();}) 
+b6 = document.querySelector("#b6"); let b6sound = new Audio('/src/sounds/B6.mp3'); 
+b6.addEventListener("click", function() {b6sound.play();}); b6.addEventListener("mouseup", function() {b6sound.pause();b6sound.load();}) 
+c6 = document.querySelector("#c6"); let c6sound = new Audio('/src/sounds/C6.mp3'); 
+c6.addEventListener("click", function() {c6sound.play();}); c6.addEventListener("mouseup", function() {c6sound.pause();c6sound.load();})  
+d6 = document.querySelector("#d6"); let d6sound = new Audio('/src/sounds/D6.mp3'); 
+d6.addEventListener("click", function() {d6sound.play();}); d6.addEventListener("mouseup", function() {d6sound.pause();d6sound.load();})
+e6 = document.querySelector("#e6"); let e6sound = new Audio('/src/sounds/E6.mp3'); 
+e6.addEventListener("click", function() {e6sound.play();}); e6.addEventListener("mouseup", function() {e6sound.pause();e6sound.load();})
+f6 = document.querySelector("#f6"); let f6sound = new Audio('/src/sounds/F6.mp3'); 
+f6.addEventListener("click", function() {f6sound.play();}); f6.addEventListener("mouseup", function() {f6sound.pause();f6sound.load();})  
+g6 = document.querySelector("#g6"); let g6sound = new Audio('/src/sounds/G6.mp3'); 
+g6.addEventListener("click", function() {g6sound.play();}); g6.addEventListener("mouseup", function() {g6sound.pause();g6sound.load();})
+
+// OCTAVE 1 SHARPS, LINES 99-106
+c1s = document.querySelector("#c1s"); let c1ssound = new Audio('/src/sounds/Db1.mp3'); 
+c1s.addEventListener("click", function() {c1ssound.play();}); c1s.addEventListener("mouseup", function() {c1ssound.pause();c1ssound.load();}) 
+d1s = document.querySelector("#d1s"); let d1ssound = new Audio('/src/sounds/Eb1.mp3'); 
+d1s.addEventListener("click", function() {d1ssound.play();}); d1s.addEventListener("mouseup", function() {d1ssound.pause();d1ssound.load();}) 
+f1s = document.querySelector("#f1s"); let f1ssound = new Audio('/src/sounds/Gb1.mp3'); 
+f1s.addEventListener("click", function() {f1ssound.play();}); f1s.addEventListener("mouseup", function() {f1ssound.pause();f1ssound.load();})  
+g1s = document.querySelector("#g1s"); let g1ssound = new Audio('/src/sounds/Ab1.mp3'); 
+g1s.addEventListener("click", function() {g1ssound.play();}); g1s.addEventListener("mouseup", function() {g1ssound.pause();g1ssound.load();})
+
+// OCTAVE 2 SHARPS, LINES 109-118
+c2s = document.querySelector("#c2s"); let c2ssound = new Audio('/src/sounds/Db2.mp3'); 
+c2s.addEventListener("click", function() {c2ssound.play();}); c2s.addEventListener("mouseup", function() {c2ssound.pause();c2ssound.load();}) 
+d2s = document.querySelector("#d2s"); let d2ssound = new Audio('/src/sounds/Eb2.mp3'); 
+d2s.addEventListener("click", function() {d2ssound.play();}); d2s.addEventListener("mouseup", function() {d2ssound.pause();d2ssound.load();}) 
+f2s = document.querySelector("#f2s"); let f2ssound = new Audio('/src/sounds/Gb2.mp3'); 
+f2s.addEventListener("click", function() {f2ssound.play();}); f2s.addEventListener("mouseup", function() {f2ssound.pause();f2ssound.load();})  
+g2s = document.querySelector("#g2s"); let g2ssound = new Audio('/src/sounds/Ab2.mp3'); 
+g2s.addEventListener("click", function() {g2ssound.play();}); g2s.addEventListener("mouseup", function() {g2ssound.pause();g2ssound.load();})
+a2s = document.querySelector("#a2s"); let a2ssound = new Audio('/src/sounds/Bb2.mp3'); 
+a2s.addEventListener("click", function() {a2ssound.play();}); a2s.addEventListener("mouseup", function() {a2ssound.pause();a2ssound.load();})
+
+// OCTAVE 3 SHARPS, LINES 121-130
+c3s = document.querySelector("#c3s"); let c3ssound = new Audio('/src/sounds/Db3.mp3'); 
+c3s.addEventListener("click", function() {c3ssound.play();}); c3s.addEventListener("mouseup", function() {c3ssound.pause();c3ssound.load();}) 
+d3s = document.querySelector("#d3s"); let d3ssound = new Audio('/src/sounds/Eb3.mp3'); 
+d3s.addEventListener("click", function() {d3ssound.play();}); d3s.addEventListener("mouseup", function() {d3ssound.pause();d3ssound.load();}) 
+f3s = document.querySelector("#f3s"); let f3ssound = new Audio('/src/sounds/Gb3.mp3'); 
+f3s.addEventListener("click", function() {f3ssound.play();}); f3s.addEventListener("mouseup", function() {f3ssound.pause();f3ssound.load();})  
+g3s = document.querySelector("#g3s"); let g3ssound = new Audio('/src/sounds/Ab3.mp3'); 
+g3s.addEventListener("click", function() {g3ssound.play();}); g3s.addEventListener("mouseup", function() {g3ssound.pause();g3ssound.load();})
+a3s = document.querySelector("#a3s"); let a3ssound = new Audio('/src/sounds/Bb3.mp3'); 
+a3s.addEventListener("click", function() {a3ssound.play();}); a3s.addEventListener("mouseup", function() {a3ssound.pause();a3ssound.load();})
+
+// OCTAVE 4 SHARPS, LINES 133-142
+c4s = document.querySelector("#c4s"); let c4ssound = new Audio('/src/sounds/Db4.mp3'); 
+c4s.addEventListener("click", function() {c4ssound.play();}); c4s.addEventListener("mouseup", function() {c4ssound.pause();c4ssound.load();}) 
+d4s = document.querySelector("#d4s"); let d4ssound = new Audio('/src/sounds/Eb4.mp3'); 
+d4s.addEventListener("click", function() {d4ssound.play();}); d4s.addEventListener("mouseup", function() {d4ssound.pause();d4ssound.load();}) 
+f4s = document.querySelector("#f4s"); let f4ssound = new Audio('/src/sounds/Gb4.mp3'); 
+f4s.addEventListener("click", function() {f4ssound.play();}); f4s.addEventListener("mouseup", function() {f4ssound.pause();f4ssound.load();})  
+g4s = document.querySelector("#g4s"); let g4ssound = new Audio('/src/sounds/Ab4.mp3'); 
+g4s.addEventListener("click", function() {g4ssound.play();}); g4s.addEventListener("mouseup", function() {g4ssound.pause();g4ssound.load();})
+a4s = document.querySelector("#a4s"); let a4ssound = new Audio('/src/sounds/Bb4.mp3'); 
+a4s.addEventListener("click", function() {a4ssound.play();}); a4s.addEventListener("mouseup", function() {a4ssound.pause();a4ssound.load();})
+
+// OCTAVE 5 SHARPS, LINES 145-154
+c5s = document.querySelector("#c5s"); let c5ssound = new Audio('/src/sounds/Db5.mp3'); 
+c5s.addEventListener("click", function() {c5ssound.play();}); c5s.addEventListener("mouseup", function() {c5ssound.pause();c5ssound.load();}) 
+d5s = document.querySelector("#d5s"); let d5ssound = new Audio('/src/sounds/Eb5.mp3'); 
+d5s.addEventListener("click", function() {d5ssound.play();}); d5s.addEventListener("mouseup", function() {d5ssound.pause();d5ssound.load();}) 
+f5s = document.querySelector("#f5s"); let f5ssound = new Audio('/src/sounds/Gb5.mp3'); 
+f5s.addEventListener("click", function() {f5ssound.play();}); f5s.addEventListener("mouseup", function() {f5ssound.pause();f5ssound.load();})  
+g5s = document.querySelector("#g5s"); let g5ssound = new Audio('/src/sounds/Ab5.mp3'); 
+g5s.addEventListener("click", function() {g5ssound.play();}); g5s.addEventListener("mouseup", function() {g5ssound.pause();g5ssound.load();})
+a5s = document.querySelector("#a5s"); let a5ssound = new Audio('/src/sounds/Bb5.mp3'); 
+a5s.addEventListener("click", function() {a5ssound.play();}); a5s.addEventListener("mouseup", function() {a5ssound.pause();a5ssound.load();})
+
+// OCTAVE 6 SHARPS, LINES 157-164
+c6s = document.querySelector("#c6s"); let c6ssound = new Audio('/src/sounds/Db6.mp3'); 
+c6s.addEventListener("click", function() {c6ssound.play();}); c6s.addEventListener("mouseup", function() {c6ssound.pause();c6ssound.load();}) 
+d6s = document.querySelector("#d6s"); let d6ssound = new Audio('/src/sounds/Eb6.mp3'); 
+d6s.addEventListener("click", function() {d6ssound.play();}); d6s.addEventListener("mouseup", function() {d6ssound.pause();d6ssound.load();}) 
+f6s = document.querySelector("#f6s"); let f6ssound = new Audio('/src/sounds/Gb6.mp3'); 
+f6s.addEventListener("click", function() {f6ssound.play();}); f6s.addEventListener("mouseup", function() {f6ssound.pause();f6ssound.load();})  
+g6s = document.querySelector("#a6s"); let g6ssound = new Audio('/src/sounds/Bb6.mp3'); 
+g6s.addEventListener("click", function() {g6ssound.play();}); a6s.addEventListener("mouseup", function() {g6ssound.pause();g6ssound.load();})
